bb59b0603f22a40d82207d687a1b832d2fae2de7
bzorn <v.klimenko.2137@gmail.com>
2026-04-30 20:59:53 +0300

    fix types

--- src/main/java/zabi/minecraft/extraalchemy/client/screen/ModScreens.java
+++ src/main/java/zabi/minecraft/extraalchemy/client/screen/ModScreens.java
@@ -3,11 +3,17 @@ package zabi.minecraft.extraalchemy.client.screen;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import zabi.minecraft.extraalchemy.screen.ModScreenHandlerTypes;
 import zabi.minecraft.extraalchemy.screen.potion_bag.PotionBagScreen;
+import zabi.minecraft.extraalchemy.screen.potion_bag.PotionBagScreenHandler;
 
 public class ModScreens {
-	
-	public static void init() {
-		HandledScreens.register(ModScreenHandlerTypes.POTION_BAG, (handler, inventory, title) -> new PotionBagScreen(title, handler, inventory));
-	}
+
+    public static void init() {
+        HandledScreens.<PotionBagScreenHandler, PotionBagScreen>register(
+                ModScreenHandlerTypes.POTION_BAG,
+                (
+                        handler, inventory, title
+                ) -> new PotionBagScreen(title, handler, inventory)
+        );
+    }
 
 }
